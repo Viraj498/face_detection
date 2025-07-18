29746cecdc368bfd8230b3208d58ffa5ea6fc57f
Viraj498 <devkarviraj498@gmail.com>
2025-07-18 16:11:15 +0530

    Created app.py file include the function analyze()

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from deepface import DeepFace
+import os
+import cv2
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analyze',methods = ['POST'])
+def analyze():
+    if 'image' not in request.files:
+        return "No File Found", 400
+    file = request.files['image']
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+    
+    try:
+        result = DeepFace.analyze(file_path, actions = ['emotion'], enforce_detection=False)
+        emotion = result[0]['emotion']
+        
+    except Exception as e:
+        return f"Error : {str(e)}"
+    
+    return render_template('result,html',emotion=emotion, image_path = file.filename)
+
+if __name__ == '__main__':
+    app.run(debug = True)
